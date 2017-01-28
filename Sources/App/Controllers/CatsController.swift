74895f59c0f0f0c1e81e94fcb3f48d955148839f
Joesus <joesusnick@gmail.com>
2017-01-28 16:26:40 -0700

    adds a controller

--- Sources/App/Controllers/CatsController.swift
+++ Sources/App/Controllers/CatsController.swift
@@ -1,9 +1,35 @@
-//
-//  CatsController.swift
-//  catV1
-//
-//  Created by Joesus on 1/28/17.
-//
-//
+import Vapor
+import HTTP
+import VaporPostgreSQL
 
-import Foundation
+final class CatsController {
+
+    func addRoutes(drop: Droplet) {
+        drop.get("version", handler: version)
+        drop.get("cats", handler: cats)
+        drop.post("cat", handler: cat)
+    }
+
+    func version(request: Request) throws -> ResponseRepresentable {
+        if let db = drop.database?.driver as? PostgreSQLDriver {
+            let version = try db.raw("SELECT version()")
+            return try JSON(node: version)
+        } else {
+            return "No db connection"
+        }
+    }
+
+    func cats(request: Request) throws -> ResponseRepresentable {
+        var cat = try Cat(node: request.json)
+        try cat.save()
+        return cat
+    }
+
+    func cat(request: Request) throws -> ResponseRepresentable {
+        var cat = try Cat(node: request.json)
+        try cat.save()
+        return cat
+    }
+
+
+}
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -6,31 +6,7 @@ let drop = Droplet()
 try drop.addProvider(VaporPostgreSQL.Provider.self)
 drop.preparations = [Cat.self]
 
-drop.get("version") { request in
-    print("why not work?")
-    if let db = drop.database?.driver as? PostgreSQLDriver {
-        print("is there driver?")
-        let version = try db.raw("SELECT version()")
-        return try JSON(node: version)
-    } else {
-        return "Driver did not initialize"
-    }
-}
-
-drop.get("test") { request in
-    var cat = Cat(name: "First", adoptable: true, age: 1, city: "Denver", state: "CO", cutenessLevel: 4)
-    try cat.save()
-    return try JSON(node: Cat.all().makeNode())
-}
-
-drop.post("cat") { request in
-    var cat = try Cat(node: request.json)
-    try cat.save()
-    return cat
-}
-
-drop.get("cats") { request in
-    return try JSON(node: Cat.all().makeNode())
-}
+let catsController = CatsController()
+catsController.addRoutes(drop: drop)
 
 drop.run()

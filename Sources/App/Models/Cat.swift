1eefe9db580e31771fa003dee94c443643136f8a
Joesus <joesusnick@gmail.com>
2017-01-28 13:27:40 -0700

    adds first few properties and get all cats endpoint

---
+++ Sources/App/Models/Cat.swift
@@ -0,0 +1,81 @@
+import Vapor
+
+typealias Favorites = [String: Any]
+
+struct CatTableKeys {
+    static let tableName = "cats"
+    static let id = "id"
+    static let name = "name"
+    static let adoptable = "adoptable"
+    static let age = "age"
+    static let city = "city"
+    static let state = "state"
+    static let cutenessLevel = "cutenessLevel"
+    static let gender = "gender"
+    static let greeting = "greeting"
+    static let pictureURL = "pictureURL"
+    static let weight = "weight"
+}
+
+final class Cat: Model {
+
+    var id: Node?
+    var exists: Bool = false
+
+    var name: String?
+    var adoptable: Bool
+    var age: Int? = 0
+    var city: String?
+    var state: String?
+    var cutenessLevel: Int?
+
+    init(name: String? = "", adoptable: Bool = false, age: Int?, city: String?, state: String?, cutenessLevel: Int?) {
+        self.id = nil
+        self.name = name
+        self.adoptable = adoptable
+        self.age = age
+        self.city = city
+        self.state = state
+        if let cutenessLevel = cutenessLevel {
+            self.cutenessLevel = cutenessLevel
+        }
+    }
+
+    init(node: Node, in context: Context) throws {
+        self.id = try node.extract(CatTableKeys.id)
+        self.name = try node.extract(CatTableKeys.name)
+        self.adoptable = try node.extract(CatTableKeys.adoptable)
+        self.age = try node.extract(CatTableKeys.age)
+        self.city = try node.extract(CatTableKeys.city)
+        self.state = try node.extract(CatTableKeys.state)
+        self.cutenessLevel = try node.extract(CatTableKeys.cutenessLevel)
+    }
+
+    func makeNode(context: Context) throws -> Node {
+        return try Node(node: [
+            CatTableKeys.id: id,
+            CatTableKeys.name: name,
+            CatTableKeys.adoptable: adoptable,
+            CatTableKeys.age: age,
+            CatTableKeys.city: city,
+            CatTableKeys.state: state,
+            CatTableKeys.cutenessLevel: cutenessLevel
+        ])
+    }
+
+    static func prepare(_ database: Database) throws {
+        try database.create(CatTableKeys.tableName) { cats in
+            cats.id()
+            cats.string(CatTableKeys.name)
+            cats.bool(CatTableKeys.adoptable)
+            cats.int(CatTableKeys.age)
+            cats.string(CatTableKeys.city)
+            cats.string(CatTableKeys.state)
+            cats.int(CatTableKeys.cutenessLevel)
+        }
+    }
+
+    static func revert(_ database: Database) throws {
+        try database.delete(CatTableKeys.tableName)
+    }
+}
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -4,6 +4,7 @@ import VaporPostgreSQL
 let drop = Droplet()
 
 try drop.addProvider(VaporPostgreSQL.Provider.self)
+drop.preparations = [Cat.self]
 
 drop.get("version") { request in
     print("why not work?")
@@ -15,6 +16,16 @@ drop.get("version") { request in
         return "Driver did not initialize"
     }
 
+}
+
+drop.get("cats") { request in
+    return try JSON(node: Cat.all().makeNode())
+}
+
+drop.get("test") { request in
+    var cat = Cat(name: "First", adoptable: true, age: 1, city: "Denver", state: "CO", cutenessLevel: 4)
+    try cat.save()
+    return try JSON(node: Cat.all().makeNode())
 }
 
 drop.run()
